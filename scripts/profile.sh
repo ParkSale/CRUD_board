6717e58cb3a28712134b526a22f1a1d787f17e28
ParkSale <34956785+ParkSale@users.noreply.github.com>
2020-05-19 01:40:48 +0900

    Nginx 무중단배포

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -3,7 +3,7 @@
 function find_idle_profile(){
   RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
-  if [ ${RESPONSE_CODE} -ge 400]
+  if [ ${RESPONSE_CODE} -ge 400 ]
 
   then
     CURRENT_PROFILE=real2
@@ -26,7 +26,7 @@ function find_idle_profile(){
 function find_idle_port(){
   IDLE_PROFILE=$(find_idle_profile)
 
-  if [ ${IDLE_PROFILE} == real1]
+  if [ ${IDLE_PROFILE} == real1 ]
   then
     echo "8081"
   else
