c1d23f5bbfae77f3b43d5ca58709782776eff4fb
ParkSale <34956785+ParkSale@users.noreply.github.com>
2020-05-22 10:01:13 +0900

    파비콘 설정

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -40,5 +41,9 @@ public class HomeController {
             model.addAttribute("state","fail");
         }
         return "home";
+    }
+    @RequestMapping("favicon.ico")
+    String favicon() {
+        return "forward:/favicon.ico";
     }
 }
